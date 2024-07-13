b5edc55548cd1d6e02df058e416ced70f20b5a74
lawden <lawtaden@gmail.com>
2024-07-13 09:57:30 +0200

    index names

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,7 @@
 -- SQL script to create an index idx_name_first on the table names for the first letter of name
 
 -- Drop the index if it already exists
-DROP INDEX IF EXISTS idx_name_first ON names;
+DROP INDEX IF EXISTS index_name_first ON names;
 
 -- Create the index
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX index_name_first ON names (LEFT(name, 1));
