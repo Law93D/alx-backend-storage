3b52e7a0c16b9f5e5e2fe847611bfe0c583498b6
lawden <lawtaden@gmail.com>
2024-07-13 10:49:38 +0200

    div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,14 +4,13 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS DECIMAL(10, 4)
 BEGIN
-    DECLARE result DECIMAL(10, 4);
+    DECLARE result 
+	DECIMAL(10, 4);
 
-    IF b = 0 THEN
-        SET result = 0;
-    ELSE
-        SET result = a / b;
-    END IF;
-
-    RETURN result;
+    IF b = 0
+	THEN SET result = 0;
+	ELSE SET result = a / b;
+		END IF;
+		RETURN result;
 END$$
 DELIMITER ;
