2cd941aca0a9df4862643153c5c3400e474e17d5
lawden <lawtaden@gmail.com>
2024-07-13 08:45:01 +0200

    bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,27 +5,29 @@
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
 CREATE PROCEDURE AddBonus (
-    IN p_user_id INT,
-    IN p_project_name VARCHAR(255),
-    IN p_score INT
+    user_id INT,
+    project_name VARCHAR(255),
+    score FLOAT
 )
 BEGIN
-	DECLARE v_project_id INT;
+	DECLARE project_count INT DEFAULT 0;
+	DECLARE project_id INT DEFAULT 0;
 
-	SELECT id INTO v_project_id FROM projects WHERE name = p_project_name;
-	IF v_project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (p_project_name);
-        SET v_project_id = LAST_INSERT_ID();
-	END IF;
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (p_user_id, v_project_id, p_score);	
-    UPDATE users u
-    SET u.average_score = (
-        SELECT AVG(c.score)
-        FROM corrections c
-        WHERE c.user_id = p_user_id
-    )
-    WHERE u.id = p_user_id;
-    
+	SELECT COUNT(id)
+		INTO project_count
+		FROM projects
+		WHERE name = project_name;
+
+	IF project_count = 0 THEN
+		INSERT INTO projects (name)
+			VALUES (project_name);
+        END IF;
+	SELECT id
+		INTO project_id
+		FROM projects
+		WHERE name = project_name;
+	INSERT INTO
+corrections(user_id, project_id, score)
+		VALUES (user_id, project_id, score);
 END$$
 DELIMITER ;
