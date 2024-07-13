635d86e48dfb3d5bee868128392e4224303cce59
lawden <lawtaden@gmail.com>
2024-07-13 10:51:11 +0200

    div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,23 @@
 -- Drop the function if it already exists
 DROP FUNCTION IF EXISTS SafeDiv;
+
+-- Delimiter change to handle function creation
 DELIMITER $$
+
+-- Create the function
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS DECIMAL(10, 4)
 BEGIN
-    DECLARE result 
-	DECIMAL(10, 4);
+    DECLARE result DECIMAL(10, 4);
 
-    IF b = 0
-	THEN SET result = 0;
-	ELSE SET result = a / b;
-		END IF;
-		RETURN result;
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = CAST(a AS DECIMAL(10, 4)) / CAST(b AS DECIMAL(10, 4));
+    END IF;
+
+    RETURN result;
 END$$
+
+-- Reset the delimiter to default
 DELIMITER ;
