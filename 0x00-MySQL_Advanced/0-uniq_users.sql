1b9e2be612825c583568b2b3fa2dc03e72f006e2
lawden <lawtaden@gmail.com>
2024-07-13 07:41:38 +0200

    uniq users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- Creates a new table users even if it exists.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	id INT NOT NULL AUTO_INCREAMENT
-PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
