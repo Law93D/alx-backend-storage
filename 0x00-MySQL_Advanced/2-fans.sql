780d04625978badcbb527b56ef7c953c92c8077f
lawden <lawtaden@gmail.com>
2024-07-13 08:02:09 +0200

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
 -- script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT u.id, u.email, u.name, COUNT(l.id) AS like_count
-FROM users u
-JOIN likes l ON u.id = l.user_id
-GROUP BY u.id
-HAVING like_count > 10;
+SELECT origin, SUM(fans) AS nb_fans
+	FROM metal_bands
+	GROUP BY origin
+	ORDER BY nb_fans DESC;
