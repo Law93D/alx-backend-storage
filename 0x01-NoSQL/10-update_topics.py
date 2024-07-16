671ef0085b5a3902df7eb5a487e06edf63a687c2
lawden <lawtaden@gmail.com>
2024-07-16 18:40:33 +0200

    changes doc topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,34 +1,20 @@
 #!/usr/bin/env python3
 """
-Module for updating a document in a MongoDB collection.
+Module for updating the topics of a school document in a MongoDB collection
 """
 
 from pymongo import MongoClient
 
-
-def update_document(database_name, collection_name, query, update):
+def update_topics(mongo_collection, name, topics):
     """
-    Updates a document in a MongoDB collection.
+    Changes all topics of a school document based on the name
 
     Args:
-        database_name (str): The name of the database.
-        collection_name (str): The name of the collection.
-        query (dict): The query to find the document.
-        update (dict): The update to be applied to the document.
+    mongo_collection (pymongo.collection.Collection): pymongo collection object
+    name (str): the school name to update
+    topics (list): list of topics approached in the school
 
     Returns:
-        int: The number of documents matched and modified.
+    pymongo.results.UpdateResult: result of the update operation
     """
-    client = MongoClient()
-    db = client[database_name]
-    collection = db[collection_name]
-    result = collection.update_one(query, {"$set": update})
-    client.close()
-    return result.matched_count, result.modified_count
-
-
-if __name__ == "__main__":
-    query = {"name": "John Doe"}
-    update = {"age": 31}
-    matched, modified = update_document("my_database", "users", query, update)
-    print(f"Matched {matched} document(s), modified {modified} document(s)")
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
